05a1e325b69d24221985788cdb144ee57cc31834
jordanfox1 <jordanfox9415@gmail.com>
2023-10-12 21:15:33 +1000

    dictionary get request working

--- dictionary/network/network.go
+++ dictionary/network/network.go
@@ -2,6 +2,12 @@
 
 package network
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type NetworkClient interface {
 	GetDefinition(word, url string) (string, error)
 }
@@ -11,6 +17,29 @@ type RealNetworkClient struct {
 }
 
 func (c *RealNetworkClient) GetDefinition(word, url string) (string, error) {
-	// Make the real HTTP request here and return the result.
-	return "", nil
+	client := &http.Client{}
+
+	apiUrl := fmt.Sprintf("%s%s", url, word)
+
+	// Make the HTTP GET request.
+	resp, err := client.Get(apiUrl)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Read and parse the response.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// The response body contains the definition data in JSON format.
+	// You can parse it and extract the definition as needed.
+	// For simplicity, let's assume the response body is the definition.
+	definition := string(body)
+
+	fmt.Print(definition)
+
+	return definition, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "dictionary/dictionary/user_input"
+import (
+	"dictionary/dictionary/network"
+	"dictionary/dictionary/network_interaction"
+	"dictionary/dictionary/user_input"
+	"log"
+)
 
 func main() {
 	user_input.PromptUser(user_input.WelcomePrompt)
 	input := user_input.GetUserInput()
-	user_input.ValidateUserInput(input)
+	validInput, err := user_input.ValidateUserInput(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an instance of the real network client to use it's implementation.
+	client := &network.RealNetworkClient{}
+
+	// Create an instance of the NetworkInteraction with the real client.
+	ni := network_interaction.NetworkInteraction{Client: client}
+	definition, err := ni.GetDefinition(validInput, "https://api.dictionaryapi.dev/api/v2/entries/en/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(definition)
 }
