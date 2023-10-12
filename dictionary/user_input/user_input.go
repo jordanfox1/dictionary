043fc11269e708c4fba3f2d1d9e1758535d08c54
jordanfox1 <jordanfox9415@gmail.com>
2023-10-12 20:19:37 +1000

    tests for input validation

--- dictionary/user_input/user_input.go
+++ dictionary/user_input/user_input.go
@@ -1,14 +1,19 @@
 package user_input
 
 import (
+	"errors"
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 var (
 	Equals10      = strings.Repeat("=", 10)
 	Dash10        = strings.Repeat("-", 10)
 	WelcomePrompt = fmt.Sprintf("%s %s Welcome %s %s", Dash10, Equals10, Equals10, Dash10)
+	ErrValidation = errors.New("error validating input")
+	ErrNoInput    = errors.New("no input detected")
+	ErrNoNumeric  = errors.New("please enter only letters")
 )
 
 func PromptUserForInput(prompt string) {
@@ -27,4 +32,19 @@ func GetUserInput() string {
 		fmt.Println("Error reading input:", err)
 	}
 	return input
+}
+
+func ValidateUserInput(input string) (string, error) {
+	if input == "" {
+		return "", ErrNoInput
+	}
+
+	for _, r := range input {
+		if !unicode.IsLetter(r) && !unicode.IsSpace(r) && r != '-' {
+			fmt.Printf("Error: Rune '%c' is not a letter, whitespace, or '-'.\n", r)
+			return "", ErrNoNumeric
+		}
+	}
+
+	return input, nil
 }
--- dictionary/user_input/user_input_test.go
+++ dictionary/user_input/user_input_test.go
@@ -51,3 +51,69 @@ func TestGetUserInput(t *testing.T) {
 		t.Errorf("Expected 'Test Input', but got '%s'", userInput)
 	}
 }
+
+func TestValidateUserInput(t *testing.T) {
+	type args struct {
+		input       string
+		expectedErr error
+		expected    string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			args: args{
+				input:       "",
+				expectedErr: ErrNoInput,
+				expected:    "",
+			},
+			name:    "Test Case 1",
+			wantErr: true,
+		},
+		{
+			args: args{
+				input:       "0",
+				expectedErr: ErrNoNumeric,
+				expected:    "",
+			},
+			name:    "Test Case 2",
+			wantErr: true,
+		},
+		{
+			args: args{
+				input:       "big shot",
+				expectedErr: ErrNoNumeric,
+				expected:    "big shot",
+			},
+			name:    "Test Case 3",
+			wantErr: false,
+		},
+		{
+			args: args{
+				input:       "HEY",
+				expectedErr: ErrNoNumeric,
+				expected:    "HEY",
+			},
+			name:    "Test Case 4",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			actual, err := ValidateUserInput(tt.args.input)
+			if err != nil && tt.wantErr {
+				if err != tt.args.expectedErr {
+					t.Errorf("ValidateUserInput() error = %v, wantErr %v, expected error message to be %v", err, tt.wantErr, tt.args.expectedErr)
+				}
+			}
+
+			if actual != tt.args.expected {
+				t.Errorf("ValidateUserInput() error = %v, wantErr %v, expected output to be %v", err, tt.wantErr, tt.args.expected)
+			}
+
+		})
+	}
+}
