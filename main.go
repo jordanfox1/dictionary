370355f3e26406ab3893dbe77fc62163c6535dc2
jordanfox1 <jordanfox9415@gmail.com>
2023-10-13 18:41:21 +1000

    finished dictionary file system implementation

--- main.go
+++ main.go
@@ -16,33 +16,53 @@ var fileStorage = storage.NewFileStorage()
 
 func main() {
 	user_input.PromptUser(user_input.WelcomePrompt)
-	input := user_input.GetUserInput("Enter a word: ")
-	validInput, err := user_input.ValidateUserInput(input)
+
+	inputOp := user_input.GetUserInput(user_input.EnterOperationPrompt)
+	validInputOp, err := user_input.ValidateUserOpInput(inputOp)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	inputWord := user_input.GetUserInput("Enter a word: ")
+	validInputWord, err := user_input.ValidateUserInput(inputWord)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if validInputOp == "l" {
+		getNewDef(validInputWord)
+	}
+
+	if validInputOp == "g" {
+		gottenDef, err := getSavedDef(validInputWord)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Got definition:")
+		fmt.Println(gottenDef)
+	}
+
+}
+
+func getNewDef(word string) models.CustomDefinition {
 	// Create an instance of the real network client to use it's implementation.
 	client := &network.RealNetworkClient{}
 
 	// Create an instance of the NetworkInteraction with the real client.
 	ni := network_interaction.NetworkInteraction{Client: client}
-	rawDefinition, err := ni.GetDefinition(validInput, "https://api.dictionaryapi.dev/api/v2/entries/en/")
+	rawDefinition, err := ni.GetDefinition(word, "https://api.dictionaryapi.dev/api/v2/entries/en/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	def := tools.FormatDefinition(rawDefinition)
-	gottenDef, err := getSavedDef(def.Word)
+	err = storage_interaction.SaveDefInDB(fileStorage, def)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(gottenDef)
-}
 
-func saveDef(def models.CustomDefinition) {
-	user_input.PromptUser(user_input.EnterOperationPrompt)
-	storage_interaction.SaveDefInDB(fileStorage, def)
+	fmt.Printf("Definition for %s has been saved!", word)
+	return def
 }
 
 func getSavedDef(word string) (models.CustomDefinition, error) {
